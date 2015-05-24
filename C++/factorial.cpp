2f8c56d9260bbf4681a6e1bb70376fe0df27dbf4
JohnnyP <johnnnyppp@gmail.com>
2015-05-24 16:04:02 +0300

    Initial release
    Testing...

---
+++ C++/factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int factorial(int i);
+
+int main()
+{
+	int i;
+
+	std::cout<<"Enter positive integer: ";
+	std::cin>>i;
+	std::cout<<"\nFactorial of the given number is: " <<factorial(i)<<"\n";
+
+	return 0;
+}
+
+int factorial(int i)
+{
+	if(i==0)
+		return 1;
+	else
+		return i*factorial(i-1);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+SourceLair project.
