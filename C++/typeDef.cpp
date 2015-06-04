74d647595eaf097503cbc6b5f3c1ea1b5624abf3
JohnnyP <johnnnyppp@gmail.com>
2015-06-04 21:19:03 +0300

    typedef function

---
+++ C++/typeDef.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct
+{
+    int a;
+    int b;
+}Mystruct;
+
+Mystruct myfun(); 
+
+int main()
+{
+    char name[30];
+    Mystruct ms2;
+    ms2 = myfun();
+    printf("val1: %d   val2: %d",ms2.a,ms2.b);
+    return 0;
+}
+
+Mystruct myfun()
+{
+    int a,b;
+    Mystruct ms;
+
+    a = 10;
+    b = 20;
+    ms.a=a;
+    ms.b=b;
+
+    return(ms);
+}
